a9b3201b7bada13e0ce3ac3b4d75ad634f4675d0
aleion <alejandro.ion.carvajal@gmail.com>
2019-04-20 10:52:13 -0400

    order model created

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_18_004330) do
+ActiveRecord::Schema.define(version: 2019_04_20_145132) do
 
   create_table "categories", force: :cascade do |t|
     t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
